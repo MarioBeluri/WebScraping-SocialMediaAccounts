ed1097d7743b5cbcea21c82beccfe19d8404f472
Bhupendra Acharya <ba127004@users.noreply.github.com>
2024-02-14 13:25:25 +0100

    Update selenium_driver_util.py

--- selenium_driver_util.py
+++ selenium_driver_util.py
@@ -1,64 +1,116 @@
-import os
-import pymongo
+import time
 
-from pymongo import MongoClient
+import platform
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-class MongoDBActor:
-    def __init__(self, collection_name, db_name=None):
-        self.collection_name = collection_name
-        self.db_name = db_name or "username_squat"
-        self.connection_string = "mongodb://{}:{}@{}:{}/?authSource={}".format(
-            os.environ['MONGO_USER_NAME'],
-            os.environ['MONGO_PASSWORD'],
-            os.environ['MONGO_IP'],
-            os.environ['MONGO_PORT'],
-            self.db_name
-        )
-        self.client = MongoClient(self.connection_string)
-        self.db = self.client[self.db_name]
-        self.col_name = self.db[self.collection_name]
+class PageDriver:
+    def __init__(self, url):
+        self.url = url
+        self.driver = None
 
-    def insert_data(self, data):
-        _insert = self.col_name.insert_one(data)
-        return _insert.inserted_id
+    def build_chrome_options(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.accept_untrusted_certs = True
+        chrome_options.assume_untrusted_cert_issuer = True
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-impl-side-painting")
+        chrome_options.add_argument("--disable-setuid-sandbox")
+        chrome_options.add_argument("--disable-seccomp-filter-sandbox")
+        chrome_options.add_argument("--disable-breakpad")
+        chrome_options.add_argument("--disable-cast")
+        chrome_options.add_argument("--disable-cast-streaming-hw-encoding")
+        chrome_options.add_argument("--disable-cloud-import")
+        chrome_options.add_argument("--disable-popup-blocking")
+        chrome_options.add_argument("--ignore-certificate-errors")
+        chrome_options.add_argument("--disable-session-crashed-bubble")
+        chrome_options.add_argument("--disable-ipv6")
+        chrome_options.add_argument("--allow-http-screen-capture")
+        chrome_options.add_argument("--start-maximized")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--incognito")
+        if platform.system().startswith("Linux"): # what ever you prefer 
+            chrome_options.add_argument("--headless")
 
-    # note this does not store the key, it will replace with new data
-    def replace_insert_if_not_found(self, key, data, _upsert=True):
-        replace = self.col_name.replace_one(key, data, upsert=_upsert)
-        return replace.upserted_id
+        return chrome_options
 
-    # this will store the key, and will have a key as well in data
-    def find_and_modify(self, key, data):
-        update = self.col_name.update_one(key, {'$set': data}, upsert=True)
-        return update.upserted_id
+    def get_driver(self):
+        chrome_options = self.build_chrome_options()
+        if platform.system().startswith("Linux"):
+            self.driver = webdriver.Chrome(options=chrome_options)
+        if platform.system().startswith("Darwin"):
+            self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chrome_options)
 
-    def find_one(self, param, sort_by=None):
-        if sort_by:
-            return self.col_name.find_one(param, sort=[(sort_by, pymongo.DESCENDING)])
-        else:
-            return self.col_name.find_one(param)
+        self.driver.set_window_size(1200, 803)
 
-    def distinct(self, key, filter=None):
-        if filter:
-            _found = self.col_name.distinct(key=key, filter=filter)
-        else:
-            _found = self.col_name.distinct(key=key)
-        if None in _found:
-            _found.remove(None)
-        if "" in _found:
-            _found.remove("")
-        return _found
+        self.driver.get(self.url)
+        return self.driver
 
-    def find(self, key=None, limit=None):
-        if key and limit is None:
-            found = self.col_name.find(key)
-        elif key is None and limit:
-            found = self.col_name.find({}).limit(limit)
-        elif key and limit:
-            found = self.col_name.find(key).limit(limit)
-        elif key is None and limit is None:
-            found = self.col_name.find({})
-        else:
-            raise Exception("query not supported")
-        return found
+    def scroll_to_bottom_of_page(self):
+        try:
+            lenOfPage = self.driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+            match = False
+            while not match:
+                lastCount = lenOfPage
+                time.sleep(3)
+                lenOfPage = self.driver.execute_script(
+                    "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return "
+                    "lenOfPage;")
+                if lastCount == lenOfPage:
+                    match = True
+        except:
+            pass
+
+    def try_waiting_xpath(self, x_path, wait_time=5):
+        try:
+            WebDriverWait(self.driver, wait_time).until(
+                EC.visibility_of_element_located((By.XPATH, x_path)))
+            elem = self.driver.find_element(By.XPATH, x_path)
+            return elem
+
+        except Exception as ex:
+            print("Exception occurred in getting xpath {}, msg: {}".format(x_path, ex))
+
+        return None
+
+    def try_getting_href_and_text(self, link_href="//a[@href]"):
+        _data = set()
+        try:
+            WebDriverWait(self.driver, 5).until(
+                EC.element_to_be_clickable((By.XPATH, link_href)))
+            elems = self.driver.find_elements(By.XPATH, link_href)
+            for elem in elems:
+                found_href = elem.get_attribute("href")
+                found_text = elem.text
+                _data.add((found_href, found_text))
+            return list(_data)
+        except Exception as ex:
+            print("Exception occurred while trying to read href, text".format(ex))
+
+        return None
+
+    def clean_up(self):
+        try:
+            self.driver.close()
+            self.driver.quit()
+        except Exception as ex:
+            try:
+                self.driver.close()
+                self.driver.quit()
+                print("Successfully closed browser after exception! {}".format(str(ex)))
+            except Exception as ex_2:
+                print("Unable to close browser as of some exceptions {}".format(str(ex_2)))
+
+        finally:
+            try:
+                self.driver.close()
+                self.driver.quit()
+                print("Finally closed ... Successfully closed browser at final stage!")
+            except Exception as ex:
+                print("Except on finally closed .. : {}".format(str(ex)))
